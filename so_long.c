7e62fecdaec17af4c1c3fce7b8dfc66b4850187d
Oleh Ruban <oruban@c2s19d121.42wolfsburg.de>
2024-03-10 12:07:32 +0100

    Makefile created, libft/ added

---
+++ so_long.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 13:07:14 by oruban            #+#    #+#             */
+/*   Updated: 2024/03/09 09:21:44 by oruban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// https://github.com/jotavare/42-resources?tab=readme-ov-file#02-so_long
+// https://reactive.so/post/42-a-comprehensive-guide-to-so_long
+// https://www.youtube.com/watch?v=D1DeE-Qit3M&t=17s
+
+#include "so_long.h"
+
+static void map_check(char *map_name)
+{
+	ft_printf("%s\n", map_name);
+}
+
+int main(int ac, char **av)
+{
+	(void) av;
+	
+	if (ac != 2)
+	{
+		write(2, "Error: Wrong number of arguments!\n", 34);
+		return (0);
+	}
+	map_check(av[1]);
+	return (0);
+}
+
+/* // #include "mlx/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+ 
+typedef struct s_data
+{
+	void *mlx_ptr;
+	void *win_ptr;
+} t_data;
+ 
+int on_destroy(t_data *data)
+{
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	exit(0);
+	return (0);
+}
+ 
+int on_keypress(int keysym, t_data *data)
+{
+	(void)data;
+	printf("Pressed key: %d\n", keysym);
+	return (0);
+}
+ 
+int main(void)
+{
+	t_data data;
+ 
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+ 
+	// Register key release hook
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+ 
+	// Register destroy hook
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+ 
+	// Loop over the MLX pointer
+	mlx_loop(data.mlx_ptr);
+	return (0);
+}
+ */
---
+++ so_long.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 13:07:53 by oruban            #+#    #+#             */
+/*   Updated: 2024/03/10 11:55:34 by oruban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+// # include <mlx.h>
+# include "libft/libft.h"
+# include "libft/ft_printf/ft_printf.h"
+# include "minilibx_macos/mlx.h"
+#endif  /* SO_LONG_H */
